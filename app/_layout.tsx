5d773584b2397af4291e1857c3b957ab08f4db9e
Brian <rollinglights90@gmail.com>
2025-07-23 04:16:12 +0400

    feat: implement tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitleAlign: "center",
+        }}
+      />
+    </Stack>
+  );
 }
